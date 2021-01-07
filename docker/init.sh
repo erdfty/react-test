965930799e6230b1621728bcb5d47ad2ef6baeca
hanson <hanson.hang@dbappsecurity.com.cn>
2021-01-07 11:19:40 +0800

    打包

--- docker/init.sh
+++ docker/init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-setEnv() {
-    if [ ! -d "/usr/share/nginx/html/dist/static" ] || [ ! -f "/usr/share/nginx/html/dist/static/envTemplate.js" ]; then
-        sleep 1s
-        setEnv
-    else
-        echo "我走这一步"
-    fi
-}
-setEnv
+# setEnv() {
+#     if [ ! -d "/usr/share/nginx/html/dist/static" ] || [ ! -f "/usr/share/nginx/html/dist/static/envTemplate.js" ]; then
+#         sleep 1s
+#         setEnv
+#     else
+#         echo "我走这一步"
+#     fi
+# }
+# setEnv
 set -e
 echo -e $HOST_CONFIG >> /etc/hosts
 exec "$@"
